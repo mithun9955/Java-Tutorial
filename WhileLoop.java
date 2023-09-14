dc564ccdabdc41f8452e341372c3d1b6bb7ac9b1
MithunKumarGupta <mithunkumargupta786@gmail.com>
2023-09-14 23:29:43 +0530

    Simple While Loop

---
+++ WhileLoop.java
@@ -0,0 +1,14 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i=0;
+        while(i<10){
+            System.out.println(i);
+            i++;
+        }
+        
+         // while(i<10){
+          //  System.out.println("Mithun");
+          //  i++;
+       // }
+    }
+}
