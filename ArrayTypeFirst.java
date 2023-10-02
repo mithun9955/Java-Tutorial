24c0ab6041790309c6d35743daaa6d52eeb650b4
MithunKumarGupta <mithunkumargupta786@gmail.com>
2023-10-02 16:39:01 +0530

    First Commit

---
+++ ArrayTypeFirst.java
@@ -0,0 +1,17 @@
+public class ArrayTypeFirst {
+    public static void main(String[] args) {
+        int marks[] = new int[3];
+        marks[0]=97;
+        marks[1]=98;
+        marks[2]=95;
+        // method1
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+
+        //other print method2
+        for (int i= 0 ;i<3;++i){
+            System.out.println(marks[i]);
+        }
+    }
+}
---
+++ ArrayTypeTwo.java
@@ -0,0 +1,9 @@
+public class ArrayTypeTwo {
+    public static void main(String[] args) {
+        int marks[]={98,95,97};
+
+        for(int i=0;i<3;i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
